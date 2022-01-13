46a3628cff9120c1e8e6a2bfd5c8b7218495402a
Dragunov <Fomin.f.jl@gmail.com>
2022-01-14 00:39:37 +0300

    refactoring(extracted complex equation logic to one method) added working with decimal and negative numbers added test for SimplePartOfEquation

--- CalcFromString/Parser.cs
+++ CalcFromString/Parser.cs
@@ -5,63 +5,60 @@ namespace CalcFromString
 {
     public class Parser //todo con with format provider
     {
-        public string innerParentheses { get; set; } = @"\(\w*[^\(\)]\w*\)";
-        public string outerParentheses { get; set; } = @"\(.*\)";
-        private int counter = 0;
+        private string InnerParentheses { get; set; } = @"\(\w*[^\(\)]\w*\)";
+        private string OuterParentheses { get; set; } = @"\(.*\)";
+        private string MultiplyAndDivision { get; set; } = @"\d+\,*\d*[\*|\/]\-*\d+\,*\d*";
+        private string AdditionAndSubtraction { get; set; } = @"^(\-*\d+\,*\d*[\+|\-]\d+\,*\d*)|(\d+\,*\d*[\+|\-]\d+\,*\d*)";
         
-        public string OpenParentheses(string equation) //  2*(10+(1+1))*3/6*5*4/2+3-10-5 //108
+        private int counter = 0;
+
+        public string OpenParentheses(string equation) //  -2*(10+(1+1))*3/6*5*4/2+3-10-5 //-132
         {
-            Console.WriteLine(++counter);
-            var matchParenthesesRegex = Regex.Match(equation, innerParentheses);
+            var matchParenthesesRegex = Regex.Match(equation, InnerParentheses);
             if (matchParenthesesRegex.Success)
             {
-                Console.WriteLine();
                 var expressionValue = matchParenthesesRegex.Value;
                 var expValWithoutParenthesis = expressionValue
                     .Substring(1, expressionValue.Length - 2);
                 equation = equation.Replace(expressionValue,
-                    SolveEquation(expValWithoutParenthesis));
+                    SolveComplexEquation(expValWithoutParenthesis));
                 equation = OpenParentheses(equation);
             }
 
-            return SolveEquation(equation);
+            return SolveComplexEquation(equation);
         }
 
-        public string SolveEquation(string equation) //  (2+2*3/6) todo extract method with regex args
+        public string SolveComplexEquation(string equation) 
         {
-            // var startIndex = 0;
-            // var endIndex = 0;
             string partOfEquationResult;
+            equation = SeparatePartOfEquation(equation,@"\d+\,*\d*[\*|\/]\-*\d+\,*\d*");
+            equation = SeparatePartOfEquation(equation,@"^(\-*\d+\,*\d*[\+|\-]\d+\,*\d*)|(\d+\,*\d*[\+|\-]\d+\,*\d*)");
+            return equation;
+        }
 
-            var multiplicationOrDivisionBlock = Regex.Match(equation, @"\d*[\*|\/]\d*");
+
+        public string SeparatePartOfEquation(string equation, string regex)
+        {
+            string partOfEquationResult;
+            var multiplicationOrDivisionBlock = Regex.Match(equation, regex);
             if (multiplicationOrDivisionBlock.Success)
             {
                 partOfEquationResult =
                     SolveSimplePartOfEquation(multiplicationOrDivisionBlock.Value).ToString();
-                // startIndex = singleBlockMultiplicationOrDivision.Index;
-                // endIndex = startIndex + singleBlockMultiplicationOrDivision.Length;
                 equation = equation.Replace(multiplicationOrDivisionBlock.Value, partOfEquationResult);
-                equation = SolveEquation(equation);
+                equation = SolveComplexEquation(equation);
             }
-
-            var plusOrSubtractBlock = Regex.Match(equation, @"\d*[\+|\-]\d*");
-            if (plusOrSubtractBlock.Success)
-            {
-                partOfEquationResult =
-                    SolveSimplePartOfEquation(plusOrSubtractBlock.Value).ToString();
-                // startIndex = singleBlockMatchPlusOrSubtract.Index;
-                // endIndex = startIndex + singleBlockMatchPlusOrSubtract.Length;
-                equation = equation.Replace(plusOrSubtractBlock.Value, partOfEquationResult);
-                equation = SolveEquation(equation);
-            }
-
-
             return equation;
         }
 
-        public decimal SolveSimplePartOfEquation(string equation)
+        public decimal SolveSimplePartOfEquation(string equation) // todo optimize performance(separate decimal and int matching and solving) 
         {
             var digits = Regex.Matches(equation, @"-*\d+\,*\d*");
+            if (digits.Count != 2)
+            {
+                throw new ArgumentException("in: " + equation);
+            }
+
             var first = Decimal.Parse(digits[0].Value);
             var second = Decimal.Parse(digits[1].Value);
             var oper = Regex.Match(equation, @"\*|\/|\+");
@@ -70,14 +67,48 @@ namespace CalcFromString
                 oper = Regex.Match(equation, @"\-");
             }
 
+
             return oper.Value switch //todo what is several case for one answer with arrow syntax?
             {
-                "+" => (first + second),
-                "-" => (first + second),
-                "*" => (first * second),
-                "/" => (first / second),
+                "+" => checked(first + second),
+                "-" => checked(first + second),
+                "*" => checked(first * second),
+                "/" => checked(first / second),
                 _ => throw new ArgumentException("in: " + equation)
             };
         }
     }
 }
+
+
+// public string SolveEquation(string equation) //  (2+2*3/6) todo extract method with regex args
+// {
+//     // var startIndex = 0;
+//     // var endIndex = 0;
+//     string partOfEquationResult;
+//
+//     var multiplicationOrDivisionBlock = Regex.Match(equation, @"\d*[\*|\/]\d*");
+//     if (multiplicationOrDivisionBlock.Success)
+//     {
+//         partOfEquationResult =
+//             SolveSimplePartOfEquation(multiplicationOrDivisionBlock.Value).ToString();
+//         // startIndex = singleBlockMultiplicationOrDivision.Index;
+//         // endIndex = startIndex + singleBlockMultiplicationOrDivision.Length;
+//         equation = equation.Replace(multiplicationOrDivisionBlock.Value, partOfEquationResult);
+//         equation = SolveEquation(equation);
+//     }
+//
+//     var plusOrSubtractBlock = Regex.Match(equation, @"\d*[\+|\-]\d*");
+//     if (plusOrSubtractBlock.Success)
+//     {
+//         partOfEquationResult =
+//             SolveSimplePartOfEquation(plusOrSubtractBlock.Value).ToString();
+//         // startIndex = singleBlockMatchPlusOrSubtract.Index;
+//         // endIndex = startIndex + singleBlockMatchPlusOrSubtract.Length;
+//         equation = equation.Replace(plusOrSubtractBlock.Value, partOfEquationResult);
+//         equation = SolveEquation(equation);
+//     }
+//
+//
+//     return equation;
+// }
--- CalcFromString/Program.cs
+++ CalcFromString/Program.cs
@@ -7,23 +7,19 @@ namespace CalcFromString
     {
         static void Main(string[] args) //todo problems with start from "-"
         {
-            Console.WriteLine(new Parser().OpenParentheses("2*(10+(1+1))*3/6*5*4/2+3-10-5")); //108
+            Console.WriteLine(new Parser().OpenParentheses("-2*(10+(1+1))*3/6*5*4/2+3-10-5")); //108
+        }
 
-            foreach (var match in Regex.Matches("5+2+-2*-2+3.3+2",@"\d+\.*\d*"))
+        private static void TempTest()
+        {
+            foreach (var match in Regex.Matches("-5+2-2*-2,2+3,3+2",@"^(\-*\d+\,*\d*[\+|\-]\d+\,*\d*)|(\d+\,*\d*[\+|\-]\d+\,*\d*)"))
             {
                 Console.Write(match+"  ");
             }
-
-            
-            Console.WriteLine();
-            Console.WriteLine(Decimal.Parse("2,2")*Decimal.Parse("2,2"));
-            // var equation = "1+(2+(1+2)+2)+1";
-            // var matchParenthesesRegex = Regex.Match(equation, @"\(.*\)");
-            // if (matchParenthesesRegex.Success)
-            // {
-                // var value = matchParenthesesRegex.Value;
-                // Console.WriteLine(equation.Replace(value,value.Substring(1,value.Length-2)));
-            // }
+            foreach (var match in Regex.Matches("-5+2-2*-2,2+3,3+2",@"\d+\,*\d*[\+|\-]\d+\,*\d*"))
+            {
+                Console.Write(match+"  ");
+            }
         }
     }
 }
